e1f320b1f52c4dc6fb5c7f790a2d7adc36e3bbf7
Manan Sharma <manan140306@gmail.com>
2026-08-03 19:37:43 +0530

    added ,

--- app/commands/doctor/page.tsx
+++ app/commands/doctor/page.tsx
@@ -4,7 +4,7 @@ import {Metadata} from 'next';
 export const metadata : Metadata = {
     metadataBase: new URL("https://docs.cxgrd.com"),
     title : 'Command - doctor | CXGRD Docs',
-    description : 'Check the readiness and availability of tools before starting strict checks'
+    description : 'Check the readiness and availability of tools before starting strict checks',
     alternates: {
       canonical: "/commands/doctor",
     }
